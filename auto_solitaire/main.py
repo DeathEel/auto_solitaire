1c7ab56cdd97d986abef8404d4ed2764d44978f1
deatheel <kevinng2003@gmail.com>
2025-08-15 04:30:54 -0700

    <feat>(main): Add method calls to perform initial game update.
    All 52 Card objects are initialised into a set of unfound cards. Game screen is captured, cards and their positions are identified, and game state is updated. Found cards will be removed from the unfound cards.

--- auto_solitaire/main.py
+++ auto_solitaire/main.py
@@ -1,13 +1,50 @@
 import cv2
-import numpy as np
-from PIL import Image
-import subprocess
-import adb
-import game
+from adb import Screen
+from game import GameState, Card
 
-img = adb.capture_screen()
-cv2.imshow("Screen", img)
-cv2.waitKey(0)
+'''
+Foundation =	(84,	392)
+				(238,	392)
+				(390,	392)
+				(542,	392)
 
-cards_found = adb.find_card(img, "templates/AceHearts.png")
-print(cards_found)
+Waste =			(780,	392)
+
+Stock =			(1000,	392)
+
+Tableau =		(84,	y)
+				(238,	y)
+				(390,	y)
+				(542,	y)
+				(694,	y)
+				(846,	y)
+				(1000,	y)
+'''
+
+def main():
+    suits = ["H", "S", "C", "D"]
+    values = ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"]
+    template_paths = [f"data/{value}{suit}.png" for value in values for suit in suits]
+    
+    screen = Screen()
+    game = GameState()
+
+    # Initialise all 52 Card objects to a list
+    unfound_cards = set(Card(path) for path in template_paths)
+
+    # Initial screen capture, card find, and game update
+    img = screen.capture()
+    
+    found_cards = game.find_cards(img, unfound_cards)
+    
+    game.update_state(found_cards)
+    print(", ".join(f"{card.rank}{card.suit}" for card in unfound_cards))
+    print(", ".join(f"{card.rank}{card.suit}" for card in found_cards))
+    
+    game.print_state()
+    
+    cv2.imshow("Screen", img)
+    cv2.waitKey(0)
+
+if __name__ == "__main__":
+    main()
