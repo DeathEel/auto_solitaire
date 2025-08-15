62a4fc76d1985a40c917d7c0d2ef77b2cc241b35
deatheel <kevinng2003@gmail.com>
2025-08-15 06:51:51 -0700

    <feat>(adb): Allow cropping of screen capture in Screen class. Allow delay after inputs.
    When first instantiating Screen class, perform initial screen capture. When capturing screen, generate cropped images for each area. Delays after inputs allow screen captures to be clean.

--- auto_solitaire/adb.py
+++ auto_solitaire/adb.py
@@ -1,10 +1,15 @@
 import cv2
 import numpy as np
 import subprocess
+import time
 
 class Screen:
     def __init__(self):
-        pass
+        self.full_img = None
+        self.tableau_img = None
+        self.foundation_img = None
+        self.waste_img = None
+        self.capture()
 
     def capture(self):
         result = subprocess.run(
@@ -12,10 +17,15 @@ class Screen:
 	        stdout=subprocess.PIPE
     	)
         img_array = np.frombuffer(result.stdout, np.uint8)
-        return cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+        self.full_img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+        self.tableau_img = self.full_img[550:, :]
+        self.foundation_img = self.full_img[:550, :610]
+        self.waste_img = self.full_img[:550, 610:910]
 
     def tap(self, x, y):
         subprocess.run(["adb", "shell", "input", "tap", str(x), str(y)])
+        time.sleep(0.5)
 
     def swipe(self, x1, y1, x2, y2, duration_ms=300):
         subprocess.run(["adb", "shell", "input", "swipe", str(x1), str(y1), str(x2), str(y2), str(duration_ms)])
+        time.sleep(0.5)
