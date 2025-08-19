918d429f577c80fafa97b19ee1d369b7fbd43752
deatheel <kevinng2003@gmail.com>
2025-08-19 01:26:20 -0700

    <refactor>(constants): Redefine TABLEAU_POSITIONS to include the y coordinate.
    TABLEAU_POSITIONS is only used when a card does not exist at the
    tableau. In this case, the top row and highest y coordinate should be
    used.

--- auto_solitaire/constants.py
+++ auto_solitaire/constants.py
@@ -9,4 +9,4 @@ from positions import Position
 FOUNDATION_POSITIONS = {suit: Position(x, 392) for suit, x in zip(SUITS, [84, 238, 390, 542])}
 WASTE_POSITION = Position(780, 392)
 STOCK_POSITION = Position(1000, 392)
-TABLEAU_X_VALUES = (84, 238, 390, 542, 694, 846, 1000)
+TABLEAU_POSITIONS = (Position(x, 643) for x in (84, 238, 390, 542, 694, 846, 1000))
