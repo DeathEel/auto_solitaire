176549feaba04fa4840142407c3c3b2aa4a7e68f
deatheel <kevinng2003@gmail.com>
2025-08-19 18:08:47 -0700

    <refactor>(moves): Use Card object for source and Position object for destination of Move class. Remove mentions of waste and replace with stock. Remove waste to stock and vice versa moves.
    Since the source card rank and suits are needed, the Card object is used
    instead of the Position alone.
    
    Since the program sees all of the stock at once, it is redundant to have
    it also see the waste, which is the same as the stock for draw-one
    games.

--- auto_solitaire/moves.py
+++ auto_solitaire/moves.py
@@ -3,8 +3,8 @@ from game import Card, GameState
 from positions import Position
 
 class Move:
-    def __init__(self, src_position: Position, dst_position: Position):
-        self.src_position = src_position
+    def __init__(self, src_card: Card, dst_position: Position | None):
+        self.src_card = src_card
         self.dst_position = dst_position
 
 class MovesList:
@@ -13,11 +13,9 @@ class MovesList:
 
     def reset(self) -> None:
         self.tableau_to_tableau = []
-        self.stock_to_waste = []
-        self.waste_to_tableau = []
-        self.waste_to_stock = []
+        self.stock_to_tableau = []
         self.tableau_to_foundation = []
-        self.waste_to_foundation = []
+        self.stock_to_foundation = []
         self.foundation_to_tableau = []
 
     def _top_card(self, pile: list[Card]) -> Card | None:
@@ -36,26 +34,18 @@ class MovesList:
                     dst_card = self._top_card(dst_col)
                     if state.can_build(src_card, dst_card):
                         dst_position = self._dst_position(dst_card, C.TABLEAU_POSITIONS[dst_idx])
-                        self.tableau_to_tableau.append(Move(src_card.position, dst_position))
+                        self.tableau_to_tableau.append(Move(src_card, dst_position))
 
-        # Stock to Waste
-        if state.stock:
-            self.stock_to_waste.append(Move(C.STOCK_POSITION, None))
-
-        # Waste to Tableau
+        # Stock to Tableau
         for dst_idx, dst_col in enumerate(state.tableau):
-            # Verify waste is not empty
-            if not state.waste:
+            # Verify stock is not empty
+            if not state.stock:
                 break
-            src_card = state.waste[-1]
             dst_card = self._top_card(dst_col)
-            if state.can_build(src_card, dst_card):
-                dst_position = self._dst_position(dst_card, C.TABLEAU_POSITIONS[dst_idx])
-                self.waste_to_tableau.append(Move(src_card.position, dst_position))
-
-        # Waste to Stock
-        if state.waste and not state.stock:
-            self.waste_to_stock.append(Move(C.WASTE_POSITION, None))
+            for src_card in state.stock:
+                if state.can_build(src_card, dst_card):
+                    dst_position = self._dst_position(dst_card, C.TABLEAU_POSITIONS[dst_idx])
+                    self.waste_to_tableau.append(Move(src_card, dst_position))
 
         # Tableau to Foundation
         for src_col in state.tableau:
@@ -66,16 +56,16 @@ class MovesList:
             dst_card = self._top_card(dst_stack)
             if state.can_build_foundation(src_card, dst_card):
                 dst_position = self._dst_position(dst_card, C.FOUNDATION_POSITIONS[src_card.suit])
-                self.tableau_to_foundation.append(Move(src_card.position, dst_position))
+                self.tableau_to_foundation.append(Move(src_card, dst_position))
 
-        # Waste to Foundation
-        if state.waste:
-            src_card = state.waste[-1]
+        # Stock to Foundation
+        if state.stock:
             dst_stack = state.foundation[src_card.suit]
             dst_card = self._top_card(dst_stack)
-            if state.can_build_foundation(src_card, dst_card):
-                dst_position = self._dst_position(dst_card, C.FOUNDATION_POSITIONS[src_card.suit])
-                self.waste_to_foundation.append(Move(src_card.position, dst_position))
+            for src_card in state.stock:
+                if state.can_build_foundation(src_card, dst_card):
+                    dst_position = self._dst_position(dst_card, C.FOUNDATION_POSITIONS[src_card.suit])
+                    self.waste_to_foundation.append(Move(src_card, dst_position))
 
         # Foundation to Tableau
         for src_stack in state.foundation.values():
@@ -86,4 +76,4 @@ class MovesList:
                 dst_card = self._top_card(dst_col)
                 if state.can_build(src_card, dst_card):
                     dst_position = self._dst_position(dst_card, C.TABLEAU_POSITIONS[dst_idx])
-                    self.foundation_to_tableau.append(Move(src_card.position, dst_position))
+                    self.foundation_to_tableau.append(Move(src_card, dst_position))
