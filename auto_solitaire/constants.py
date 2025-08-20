c93d7e25d86995ffc74e1462e8efef2cc393db00
deatheel <kevinng2003@gmail.com>
2025-08-19 17:55:56 -0700

    <feat>(constants): Add Position constants for autocomplete and undo buttons.

--- auto_solitaire/constants.py
+++ auto_solitaire/constants.py
@@ -10,3 +10,5 @@ FOUNDATION_POSITIONS = {suit: Position(x, 392) for suit, x in zip(SUITS, [84, 23
 WASTE_POSITION = Position(780, 392)
 STOCK_POSITION = Position(1000, 392)
 TABLEAU_POSITIONS = (Position(x, 643) for x in (84, 238, 390, 542, 694, 846, 1000))
+AUTOCOMPLETE_POSITION = Position(540, 1800)
+UNDO_POSITION = Position(135, 2300)
